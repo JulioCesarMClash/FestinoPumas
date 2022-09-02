2b712818674642202ea9cad40671fe7e39183b3e
JulioCesarMClash <engienner.finn.jake@gmail.com>
2022-09-02 16:50:11 -0500

    Create packages Festino

---
+++ build/Hardware/robotino_node/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robotino/FestinoPumas/src/Hardware/robotino_node
+# Build directory: /home/robotino/FestinoPumas/build/Hardware/robotino_node
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ devel/share/gennodejs/ros/robotino_msgs/msg/_index.js
@@ -0,0 +1,16 @@
+
+"use strict";
+
+let AnalogReadings = require('./AnalogReadings.js');
+let EncoderReadings = require('./EncoderReadings.js');
+let PowerReadings = require('./PowerReadings.js');
+let MotorReadings = require('./MotorReadings.js');
+let DigitalReadings = require('./DigitalReadings.js');
+
+module.exports = {
+  AnalogReadings: AnalogReadings,
+  EncoderReadings: EncoderReadings,
+  PowerReadings: PowerReadings,
+  MotorReadings: MotorReadings,
+  DigitalReadings: DigitalReadings,
+};
