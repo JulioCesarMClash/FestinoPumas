a5e5f4c620a921614af8cc5620f34e46800ee980
Mitzi Anaid Ramirez Estrada <mitzi.ramest@gmail.com>
2023-01-02 14:07:46 -0600

    Removing /build and /devel dirs

--- PC_user/build/Robotino_pkgs/robotino_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robocup/FestinoPumas/PC_user/src/Robotino_pkgs/robotino_msgs
-# Build directory: /home/robocup/FestinoPumas/PC_user/build/Robotino_pkgs/robotino_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- PC_user/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/robocup/FestinoPumas/PC_user/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/robocup/FestinoPumas/PC_user/devel;/home/robocup/TMR-2022-AutoModelCar/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/robocup/FestinoPumas/PC_user/devel;/home/robocup/TMR-2022-AutoModelCar/catkin_ws/devel;/opt/ros/melodic' != '' else []
