373861fd26087ab110343d3ed725b5e893d113eb
Mitzi Anaid Ramirez Estrada <mitzi.ramest@gmail.com>
2023-01-30 20:09:10 -0600

    Movement function for GoToPlace order

--- PC_user/src/Pos_control/pos_definition/src/position_control.py
+++ PC_user/src/Pos_control/pos_definition/src/position_control.py
@@ -15,31 +15,28 @@ import tf2_ros
 from geometry_msgs.msg import *
 from std_msgs.msg import *
 
-## Publica la TF, verifica que se mueva
-## Control PID
 
-
-#class position_control:
-
-#  def __init__(self):
-#    self.piece_pos_sub = rospy.Subscriber("/redpiece_pos",PointStamped,self.callback_piece_pos)
-#    self.piece_position = [0,0,0]
+def move_distance(goal_dist, goal_angle, pub_goal_dist):
+  msg_dist = Float32MultiArray()
+  msg_dist.data = [goal_dist, goal_angle]
+  print(msg_dist)
+  pub_goal_dist.publish(msg_dist)
 
 def callback_piece_pos(data):
   global piece_position
-  piece_position = [data.point.x, data.point.y,data.point.z]
+  piece_position = [data.point.z, data.point.x,data.point.y]
   print(piece_position)
   br = tf.TransformBroadcaster()
   rate = rospy.Rate(1.0)
-  jelp = br.sendTransform((data.point.x, data.point.y, data.point.z),(0.0, 0.0, 0.0, 1.0),rospy.Time.now(),
-                     "piece_rgbd_sensor_link", "camera_link")
-  print(jelp)
+  #br.sendTransform((data.point.z, data.point.x, data.point.y), (0.0, 0.0, 0.0, 1.0),rospy.Time.now(), "piece_rgbd_sensor_link", "kinect_link")
+  br.sendTransform((data.point.z, data.point.x, data.point.y), (0.0, 0.0, 0.0, 1.0),rospy.Time.now(), "piece_rgbd_sensor_link", "camera_link")
 
 
 def main(args):
   rospy.init_node('position_control', anonymous=True)
   print("Position control - Aligning Gripper")
   piece_pos_sub = rospy.Subscriber("/redpiece_pos",PointStamped,callback_piece_pos)
+  pub_goal_dist = rospy.Publisher("/simple_move/goal_dist_angle", Float32MultiArray, queue_size=10)
   rospy.spin()
 
 if __name__ == '__main__':
