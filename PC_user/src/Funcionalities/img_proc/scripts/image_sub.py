3e5ee184dd832614f7138788004ef42230ad0403
Mitzi Anaid Ramirez Estrada <mitzi.ramest@gmail.com>
2023-02-10 17:11:33 -0600

    Arugo Tags search included, Tf of tag pulished. Speaker voice slower

--- PC_user/src/Funcionalities/img_proc/scripts/image_sub.py
+++ PC_user/src/Funcionalities/img_proc/scripts/image_sub.py
@@ -24,34 +24,6 @@ def move_distance(goal_dist, goal_angle, pub_goal_dist):
   print(msg_dist)
   pub_goal_dist.publish(msg_dist)
 
-def callback_aruco(data):
-  try:
-    image_aruco = bridge.imgmsg_to_cv2(data, "bgr8")
-  except CvBridgeError as e:
-    print(e)
-  dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_50)
-  parameters = cv2.aruco.DetectorParameters_create()
-  markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(image_aruco, dictionary, parameters=parameters)
-  color = (255, 0, 0)
-  thickness = 2
-  if(markerIds.shape[0] > 1):
-    for i in range (markerIds.shape[0]):
-      esq = markerCorners[i]
-      #shape = 1,4,2
-      sup_left = (esq[(0,0,0)],esq[(0,0,1)])
-      inf_rigth = (esq[(0,2,0)],esq[(0,2,1)])
-      image = cv2.rectangle(image_aruco, sup_left, inf_rigth, color, thickness)
-  
-  cv2.imshow("Aruco Tags", image_aruco)
-  cv2.waitKey(3)
-
-  try:
-    #position_pub.publish(piece_pose)
-    fin = 1
-  except CvBridgeError as e:
-    print(e)
-
-
 def callback_depth_points(data):
   global arr, img_bgr, Red_mask
   arr = ros_numpy.point_cloud2.pointcloud2_to_array(data)
@@ -99,7 +71,7 @@ def segment_color(img_bgr, img_xyz, hsv_mean):
   return Fil
 
 def callback_image(data):
-  global depth_img, arr, img_bgr, Red_mask
+  global depth_img, arr, img_bgr, Red_mask, msg_pub
   try:
     cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
   except CvBridgeError as e:
@@ -111,98 +83,123 @@ def callback_image(data):
   piece_pose.header.frame_id = "kinect_link"
   piece_pose.point.x, piece_pose.point.y, piece_pose.point.z = 0,0,0
 
+  aruco_pose = piece_pose
+
   ######## Looking for ARUCO TAG ########
   image_aruco = cv_image
   dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_50)
   parameters = cv2.aruco.DetectorParameters_create()
   markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(image_aruco, dictionary, parameters=parameters)
+
+  msg = 'Hello, my name is Festino'
+  msg_pub.publish(msg)
+  
   color = (255, 0, 0)
   thickness = 2
   if(markerIds.shape[0] >= 1):
     for i in range (markerIds.shape[0]):
-      esq = markerCorners[i]
+      corners = markerCorners[i]
       #shape = 1,4,2
-      sup_left = (esq[(0,0,0)],esq[(0,0,1)])
-      inf_rigth = (esq[(0,2,0)],esq[(0,2,1)])
-      image = cv2.rectangle(image_aruco, sup_left, inf_rigth, color, thickness)
+      first_corner = (corners[(0,0,0)],corners[(0,0,1)])
+      last_corner = (corners[(0,2,0)],corners[(0,2,1)])
+          
+      image = cv2.rectangle(image_aruco, first_corner, last_corner, color, thickness)
       if markerIds[i] == 13:
-        print("MPS_1 ------------- Looking for piece")
+        print("Found MPS_1 ------------- Looking for piece")
+        max_x = np.max([last_corner[0],first_corner[0]])
+        min_x = np.min([last_corner[0],first_corner[0]])
 
-    ######## Looking for ARUCO TAG ########
-    tfBuffer = tf2_ros.Buffer()
+        max_y = np.max([last_corner[1],first_corner[1]])
+        min_y = np.min([last_corner[1],first_corner[1]])
+        
+        cent = (int(max_x - (max_x-min_x)/2),int(max_y - (max_y-min_y)/2))
+        image = cv2.rectangle(image_aruco, first_corner, last_corner, (255, 255, 0), thickness)
+        cv2.circle(image_aruco, (cent), 5, (0, 255, 255), -1)
+        pos_x = float(arr[cent][0])
+        pos_y = float(arr[cent][1])
+        pos_z = float(arr[cent][2])
 
-    Masked_red = np.zeros((480, 640))
+        if not (math.isnan(pos_x) or math.isnan(pos_y) or math.isnan(pos_z)):
+            aruco_pose.point.x, aruco_pose.point.y, aruco_pose.point.z = pos_x, pos_y, pos_z
 
-    
+        br_ar = tf.TransformBroadcaster()
+        br_ar.sendTransform((aruco_pose.point.z, -aruco_pose.point.x, -aruco_pose.point.y), (0.0, 0.0, 0.0, 1.0),rospy.Time.now(), "aruco_rgbd_sensor_link", "camera_link")
 
-    img_hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
-    # --Listo-- utilizar la funcion para obtener las medias (en vez de tanto bloque de texto) 
-    # --Listo-- (puede ser dentro del callback de la nube de puntos)
-    # definir un archivo (yaml o txt o algo) donde se definan los parametros que se usaran por objeto (en este caso las medias de color)
-    # imprimor cuatos pixeles entran en cada intervalo de color y buscar el umbral minimo que corresponderia a la pieza
-    # eliminar pixeles sueltos si estan fuera de la mancha (con erode)
-    # quitar el medianBlur - erode
-    # migrar a Juskeshino 
-    # atiende a servicio que pasa la informacion necesaria y me devuelve coordenada de la pieza
+        ######## Looking for Piece ########
+        tfBuffer = tf2_ros.Buffer()
 
-    loc = cv2.findNonZero(Red_mask)
+        Masked_red = np.zeros((480, 640))
+        img_hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
-    #loc[i,0,0] : Points_i
-    #loc[i,0,1] : Points_j
-    for i in range(loc.shape[0]):
-      pos_x = float(arr[loc[i,0,1],loc[i,0,0]][0])
-      pos_y = float(arr[loc[i,0,1],loc[i,0,0]][1])
-      pos_z = float(arr[loc[i,0,1],loc[i,0,0]][2])
-      #-Sacar la media de los puntos
-      if not (math.isnan(pos_x) or math.isnan(pos_y) or math.isnan(pos_z)):
-        piece_pose.point.x, piece_pose.point.y, piece_pose.point.z = pos_x, pos_y, pos_z
-        #print(piece_pose.point,"\n")
+        # --Listo-- utilizar la funcion para obtener las medias (en vez de tanto bloque de texto) 
+        # --Listo-- (puede ser dentro del callback de la nube de puntos)
+        # definir un archivo (yaml o txt o algo) donde se definan los parametros que se usaran por objeto (en este caso las medias de color)
+        # imprimor cuatos pixeles entran en cada intervalo de color y buscar el umbral minimo que corresponderia a la pieza
+        # eliminar pixeles sueltos si estan fuera de la mancha (con erode)
+        # quitar el medianBlur - erode
+        # migrar a Juskeshino 
+        # atiende a servicio que pasa la informacion necesaria y me devuelve coordenada de la pieza
 
-    br = tf.TransformBroadcaster()
-    rate = rospy.Rate(1.0)
-    br.sendTransform((piece_pose.point.z, piece_pose.point.x, piece_pose.point.y), (0.0, 0.0, 0.0, 1.0),rospy.Time.now(), "piece_rgbd_sensor_link", "camera_link")
-    #br.sendTransform((piece_pose.point.z, -piece_pose.point.x, -piece_pose.point.y), (0.0, 0.0, 0.0, 1.0),rospy.Time.now(), "piece_rgbd_sensor_link", "camera_link")
-    
-    #listener = tf.TransformListener()
-    #listener.waitForTransform('/base_link', 'kinect_link', rospy.Time(), rospy.Duration(1.0))
-    #piece_pose2 = listener.transformPoint('base_link', piece_pose)
+        loc = cv2.findNonZero(Red_mask)
 
-    #print(piece_pose2)
-    
-    M = cv2.moments(Red_mask)
-    try: 
-      cX = int(M["m10"] / M["m00"])
-      cY = int(M["m01"] / M["m00"])
-      cv2.circle(cv_image, (cX, cY), 5, (255, 255, 255), -1)
-      cv2.putText(cv_image, "centroid_Red", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-      Masked_red = cv2.bitwise_and(cv_image, cv_image, mask = Red_mask)
-      cX_depth = cX
-      cY_depth = cY
-      cv2.circle(depth_img, (cX_depth, cY_depth), 5, (0, 255, 255), -1)
-      cv2.putText(depth_img, "centroid_Red", (cX_depth - 25, cY_depth - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    except ZeroDivisionError as e:
-      print("Object not found")
+        #loc[i,0,0] : Points_i
+        #loc[i,0,1] : Points_j
+        for i in range(loc.shape[0]):
+          pos_x = float(arr[loc[i,0,1],loc[i,0,0]][0])
+          pos_y = float(arr[loc[i,0,1],loc[i,0,0]][1])
+          pos_z = float(arr[loc[i,0,1],loc[i,0,0]][2])
+          #-Sacar la media de los puntos
+          if not (math.isnan(pos_x) or math.isnan(pos_y) or math.isnan(pos_z)):
+            piece_pose.point.x, piece_pose.point.y, piece_pose.point.z = pos_x, pos_y, pos_z
+            #print(piece_pose.point,"\n")
 
-    mean_blk = (73.37954475229168, 93.521268925739, 0.0, 0.0)
-    delta_blk = 100
-    up_h_blk = mean_blk[0] + delta_blk
-    lw_h_blk = mean_blk[0] - delta_blk
-    up_s_blk = mean_blk[1] + delta_blk
-    lw_s_blk = mean_blk[1] - delta_blk
-    up_v_blk = mean_blk[2] + delta_blk
-    lw_v_blk = mean_blk[2] - delta_blk
-    up_blk = (up_h_blk,up_s_blk,up_v_blk,0.0)
-    lw_blk = (lw_h_blk,lw_s_blk,lw_v_blk,0.0)
-    #print(lw_blk,"\n", up_blk)
-    lw_blk = (0,0,0)
-    up_blk = (360,255,50)
-    Fil_blk = cv2.inRange(img_hsv, lw_blk, up_blk)
-    Fil_blk = cv2.medianBlur(Fil_blk,9)
-  else:
-    print('Unable to find Tapita')
+        br = tf.TransformBroadcaster()
+        rate = rospy.Rate(1.0)
+        br.sendTransform((piece_pose.point.z, -piece_pose.point.x, -piece_pose.point.y), (0.0, 0.0, 0.0, 1.0),rospy.Time.now(), "piece_rgbd_sensor_link", "camera_link")
+        #br.sendTransform((piece_pose.point.z, -piece_pose.point.x, -piece_pose.point.y), (0.0, 0.0, 0.0, 1.0),rospy.Time.now(), "piece_rgbd_sensor_link", "camera_link")
+        print("Found Piece ------------- Tf Published")
+        
+        #listener = tf.TransformListener()
+        #listener.waitForTransform('/base_link', 'kinect_link', rospy.Time(), rospy.Duration(1.0))
+        #piece_pose2 = listener.transformPoint('base_link', piece_pose)
+
+        #print(piece_pose2)
+        
+        M = cv2.moments(Red_mask)
+        try: 
+          cX = int(M["m10"] / M["m00"])
+          cY = int(M["m01"] / M["m00"])
+          cv2.circle(cv_image, (cX, cY), 5, (255, 255, 255), -1)
+          cv2.putText(cv_image, "centroid_Red", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+          Masked_red = cv2.bitwise_and(cv_image, cv_image, mask = Red_mask)
+          cX_depth = cX
+          cY_depth = cY
+          cv2.circle(depth_img, (cX_depth, cY_depth), 5, (0, 255, 255), -1)
+          cv2.putText(depth_img, "centroid_Red", (cX_depth - 25, cY_depth - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        except ZeroDivisionError as e:
+          print("Object not found")
+
+        mean_blk = (73.37954475229168, 93.521268925739, 0.0, 0.0)
+        delta_blk = 100
+        up_h_blk = mean_blk[0] + delta_blk
+        lw_h_blk = mean_blk[0] - delta_blk
+        up_s_blk = mean_blk[1] + delta_blk
+        lw_s_blk = mean_blk[1] - delta_blk
+        up_v_blk = mean_blk[2] + delta_blk
+        lw_v_blk = mean_blk[2] - delta_blk
+        up_blk = (up_h_blk,up_s_blk,up_v_blk,0.0)
+        lw_blk = (lw_h_blk,lw_s_blk,lw_v_blk,0.0)
+        #print(lw_blk,"\n", up_blk)
+        lw_blk = (0,0,0)
+        up_blk = (360,255,50)
+        Fil_blk = cv2.inRange(img_hsv, lw_blk, up_blk)
+        Fil_blk = cv2.medianBlur(Fil_blk,9)
+      else:
+        print('Unable to find Tapita')
 
   cv2.imshow("Aruco Tags", image_aruco)
+  cv2.imshow("Red Piece Mask", Red_mask)
 
   
   cv2.waitKey(3)
@@ -215,7 +212,7 @@ def callback_image(data):
 def main(args):
   rospy.init_node('image_sub', anonymous=True)
 
-  global bridge, cv_depth, arr, img_bgr, position_pub, image_sub, depth_image_sub, depth_points_sub, depth_img, Red_mask
+  global bridge, cv_depth, arr, img_bgr, position_pub, image_sub, depth_image_sub, depth_points_sub, depth_img, Red_mask, msg_pub
   print("Image Processing Node - Looking for piece")
 
   bridge = CvBridge()
@@ -224,6 +221,8 @@ def main(args):
   image_sub         = rospy.Subscriber("/camera/rgb/image_color",Image,callback_image)
   depth_image_sub   = rospy.Subscriber("/camera/depth/image_raw",Image,callback_depth_image)
   depth_points_sub  = rospy.Subscriber("/camera/depth_registered/points",PointCloud2,callback_depth_points)
+  msg_pub = rospy.Publisher("/speak", String,queue_size=1)
+  msg = String()
   
   cv_depth = np.zeros((480, 640))
   depth_img = np.zeros((480, 640))
--- PC_user/src/Funcionalities/robotino_talk/scripts/speaker.py
+++ PC_user/src/Funcionalities/robotino_talk/scripts/speaker.py
@@ -7,14 +7,14 @@ from std_msgs.msg import String
 class speaker_ros:
     def __init__(self):
         self.engine = pyttsx3.init() 
-        self.engine.setProperty('rate', 180)
+        self.engine.setProperty('rate', 150)
         voices = self.engine.getProperty('voices')       
         self.engine.setProperty('voice', voices[2].id)
         self.sub = rospy.Subscriber("/speak", String, self.speakcall)
         self.msg_pub = rospy.Publisher("/speak", String, latch=True,queue_size=10)
         self.msg = String()
         self.msg.data = 'Hello, my name is Festino'
-        self.msg_pub.publish(self.msg)
+        #self.msg_pub.publish(self.msg)
 
     def speakcall(self, text):
         self.engine.say(text)
