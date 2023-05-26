1b77467eac55ced3e9c58fff26a04eff71945cf6
Joshua Martínez <joshuayz@comunidad.unam.mx>
2023-05-26 12:23:31 -0600

    Add arm files

---
+++ PC_user/src/Hardware/Arm/Festino_arm_moveit_demos/src/prueba1.cpp
@@ -0,0 +1,11 @@
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
+
+#include "Festino_arm_moveit_demos/arm.h"
+
+//#include "pincher_arm_moveit_demos/arm.h"
+
+int main(int argc, char** argv)
+{
+  
+}
