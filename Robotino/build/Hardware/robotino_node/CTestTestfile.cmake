46401885593fa20b23770a8fd0d97e90780678b7
Sango265 <sango265@gmail.com>
2024-02-22 14:38:14 -0600

    Delete Robotino/build directory

--- Robotino/build/Hardware/robotino_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robotino/FestinoPumas/Robotino/src/Hardware/robotino_node
-# Build directory: /home/robotino/FestinoPumas/Robotino/build/Hardware/robotino_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
