f34d331d579e865510560b42f2ea9753ec3ae642
Sango265 <sango265@gmail.com>
2024-02-22 14:38:26 -0600

    Delete Robotino/devel directory

--- Robotino/devel/share/robotino_msgs/cmake/robotino_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(robotino_msgs_MSG_INCLUDE_DIRS "/home/robotino/FestinoPumas/Robotino/src/Hardware/robotino_msgs/msg")
-set(robotino_msgs_MSG_DEPENDENCIES std_msgs)
