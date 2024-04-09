997442ec922dd903bf855cdef0b6f85b22f5e6c3
Sango265 <sango265@gmail.com>
2024-04-08 19:40:41 -0600

    Create install_dependencies.sh

---
+++ PC_user/install_dependencies.sh
@@ -0,0 +1,9 @@
+sudo apt install ros-noetic-random-numbers
+sudo apt update
+sudo apt install espeak
+sudo apt install python3-pip
+pip install pyttsx3
+sudo apt install mbrola
+sudo apt install mbrola-us1
+chmod +x src/HRI/pocketsphinx/install_pocket.sh
+src/HRI/pocketsphinx/install_pocket.sh
