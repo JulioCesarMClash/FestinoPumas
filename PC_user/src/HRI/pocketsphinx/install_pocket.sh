9dec4f443d1e8dc535eef37988ab7c4d398bcff1
Sango265 <sango265@gmail.com>
2024-04-08 19:38:56 -0600

    Update install_pocket.sh

--- PC_user/src/HRI/pocketsphinx/install_pocket.sh
+++ PC_user/src/HRI/pocketsphinx/install_pocket.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #necesary dependencies
-sudo apt-get install cmake
-sudo apt-get install git
+sudo apt install cmake
+sudo apt install git
 pip install pocketsphinx
 sudo apt update 
 sudo apt install swig
 sudo apt install python3-pyaudio
-sudo apt-get -y install pocketsphinx
-sudo apt install pocketphinx-en-us
+sudo apt -y install pocketsphinx
+sudo apt install pocketsphinx-en-us
 
 
 
