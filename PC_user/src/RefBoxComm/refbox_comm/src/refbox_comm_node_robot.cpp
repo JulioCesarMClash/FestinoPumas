bd60f1417d6c1f167271de08360a0ea7004b8c14
Mitzi-968 <mitzi.ramest@gmail.com>
2024-05-24 19:40:21 -0600

    había subido los cambios del nodo que no es, lo siento. Este sí es para lo de los params

--- PC_user/src/RefBoxComm/refbox_comm/src/refbox_comm_node_robot.cpp
+++ PC_user/src/RefBoxComm/refbox_comm/src/refbox_comm_node_robot.cpp
@@ -19,12 +19,12 @@
 //Lab
 #define HOST "192.168.0.255"
 
-#define TEAM_COLOR "MAGENTA"
+//#define TEAM_COLOR "MAGENTA"
 //#define TEAM_COLOR "CYAN"
 #define TEAM_NAME "Pumas"
 //#define ROBOT_NAME "Festino_macro"
 //#define ROBOT_NUMBER 6
-#define CRYPTO_KEY "randomkey"
+//#define CRYPTO_KEY "randomkey"
 #define PUBLIC_PORT_S 4444
 #define PUBLIC_PORT_R 4445
 
@@ -64,7 +64,10 @@ class Handler {
         unsigned long int m_sequence_nr_;
 
         std::string m_team_name = TEAM_NAME;
-        std::string m_robot_name;
+        //Default values 1
+        std::string m_robot_name = "Festino";
+        int m_robot_number = 1;
+        //Default values 2
         bool m_is_cyan;
 
         bool m_running;
@@ -72,38 +75,30 @@ class Handler {
 
         std::thread m_beacon_thread;
 
-        // Para recibir los parametros desde el Launch 1
-
-        int m_robot_number;
-
-        // Para recibir los parametros desde el Launch 2
-
     public:
         Handler(std::string host, int port_s, int port_r)
             : m_host(host), m_port_s(port_s)
             , m_port_r(port_r)
             , m_mr(new MessageRegister()) {
 
-
-            if(ros::param::has("robot_name")){
-                ros::param::get("robot_name", m_robot_name);
-                ROS_INFO_STREAM("entró al If :CCCCCCCCCCCCCCCC: ");    
+            if(ros::param::has("~robot_name")){
+                ros::param::get("~robot_name", m_robot_name);
             }
-            if(ros::param::has("robot_number")){
-                ros::param::get("robot_number", m_robot_number);
+            if(ros::param::has("~robot_number")){
+                ros::param::get("~robot_number", m_robot_number);
             }
 
-            ROS_INFO_STREAM("Sending NAME: " << m_robot_name << "Sending NUMBER" << m_robot_number);
+            ROS_INFO_STREAM("Sending NAME: " << m_robot_name << "\t Sending NUMBER: " << m_robot_number);
 
             m_mr->add_message_type<BeaconSignal>();
 
             m_public_peer =  std::make_shared<ProtobufBroadcastPeer>(m_host, PUBLIC_PORT_R, PUBLIC_PORT_S, m_mr);
 
-            /*
+            
             m_public_peer->signal_received().connect(
                 boost::bind(&Handler::handleRefboxMessage, this, _1, _2, _3, _4)
             );
-            */
+            
 
             m_sequence_nr_ = 0;
 
@@ -159,7 +154,7 @@ class Handler {
         // This method should notifies the refbox of a robot
         void sendBeaconSignal() {
             while (m_running) {
-                if(team_color_set) {
+                //if(team_color_set) {
                     auto cur_time = ros::Time::now();
                     std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds> now =
                     std::chrono::high_resolution_clock::now();
@@ -200,7 +195,7 @@ class Handler {
 
                     m_public_peer->send(BeaconSignal::COMP_ID, BeaconSignal::MSG_TYPE, msg);
                     std::this_thread::sleep_for(std::chrono::milliseconds(500));//ROS_INFO_STREAM("Sending: ");
-                }
+                //}
             }
         }
 };
