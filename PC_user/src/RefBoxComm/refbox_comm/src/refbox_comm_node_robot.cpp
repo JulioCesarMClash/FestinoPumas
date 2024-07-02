548dfce5fb47b8e89d8369d3b81b3cfec4b70127
Mitzi Ramirez <mitzi.ramest@gmail.com>
2024-07-02 17:44:09 -0600

    problem with beacon signal solved

--- PC_user/src/RefBoxComm/refbox_comm/src/refbox_comm_node_robot.cpp
+++ PC_user/src/RefBoxComm/refbox_comm/src/refbox_comm_node_robot.cpp
@@ -500,9 +500,9 @@ int main(int argc, char** argv)
 
         //real
         try {
-            listener_rob.waitForTransform("/base_link","/map",   
+            listener_rob.waitForTransform("/map", "/base_link",   
                                    ros::Time(0), ros::Duration(1000.0));
-            listener_rob.lookupTransform("/base_link","/map",   
+            listener_rob.lookupTransform("/map", "/base_link",   
                                    ros::Time(0), transform_rob);
         } catch (tf::TransformException ex){
             ROS_ERROR("%s",ex.what());
