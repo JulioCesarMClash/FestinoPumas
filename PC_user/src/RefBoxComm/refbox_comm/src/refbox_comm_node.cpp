6bc917bc3dd2b6fdb0b72737c57074f1fbc15327
druelas <druelas@ciencias.unam.mx>
2024-07-17 13:39:16 -0600

    changed reference from tf to log_origin

--- PC_user/src/RefBoxComm/refbox_comm/src/refbox_comm_node.cpp
+++ PC_user/src/RefBoxComm/refbox_comm/src/refbox_comm_node.cpp
@@ -1764,9 +1764,9 @@ int main(int argc, char** argv)
     tf::StampedTransform transform_rob;
 
     try{
-      listener_rob.waitForTransform("/map","/base_link",   
+      listener_rob.waitForTransform("/Log_origin","/base_link",   
                                    ros::Time(0), ros::Duration(1000.0));
-      listener_rob.lookupTransform("/map","/base_link",   
+      listener_rob.lookupTransform("/Log_origin","/base_link",   
                                    ros::Time(0), transform_rob);
     }
     catch (tf::TransformException ex){
