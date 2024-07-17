fed5e91f2451cced5a34453d57c12214dec37838
Mitzi-968 <mitzi.ramest@gmail.com>
2024-07-17 07:19:13 -0600

    Aruco det dist threshold v3

--- PC_user/src/Vision/img_proc/scripts/aruco_det_srv2.py
+++ PC_user/src/Vision/img_proc/scripts/aruco_det_srv2.py
@@ -135,7 +135,7 @@ class FindTagNode:
                       aruco_pose.point.x, aruco_pose.point.y, aruco_pose.point.z = pos_z, -pos_y+0.21, -pos_x-0.25
                       print(aruco_pose.point.x, aruco_pose.point.y, aruco_pose.point.z, '\n')
 
-                    if(aruco_pose.point.x  < 1.1):
+                    if(aruco_pose.point.x  < 0.95):
 
                       start_point = (corneru[(0,0,0)],corneru[(0,0,1)])
 
