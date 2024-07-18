247fecf4c0995195779344de7f5b9ab88ffbe922
Festino <Festino@robotino.com>
2024-07-18 03:13:42 -0600

    fixed navigation challenge

--- PC_user/src/RefBoxComm/refbox_comm/src/refbox_comm_node.cpp
+++ PC_user/src/RefBoxComm/refbox_comm/src/refbox_comm_node.cpp
@@ -692,7 +692,7 @@ ROS_INFO_STREAM("------          CRYPTO SETUP      --------- ");
         {
 
             /*TODO REMOVE */
-return;
+//return;
 
             ROS_INFO_STREAM("-------------------------------\n\n\n\n\nRecv message on private A\n\n\n\n\n------------------------------------------------------------------");
             //ROS_INFO_STREAM(""<< msg->ShortDebugString() << " typeOOOO " << msg_type);
@@ -1136,7 +1136,7 @@ pub_zone.publish(el_msg);
         void handleRefboxMessage(boost::asio::ip::udp::endpoint &endpoint, uint16_t comp_id, uint16_t msg_type, std::shared_ptr<google::protobuf::Message> msg) {
 
             /*TODO REMOVE */
-    return;
+   // return;
 //cout<< "types: " << typeid(msg).name() << " :O "<<endl;
 
 
@@ -1780,9 +1780,15 @@ int main(int argc, char** argv)
       ros::Duration(1.0).sleep();
     }
 
-    pose_y = transform_rob.getOrigin().x();
-	pose_x = transform_rob.getOrigin().y();
-
+    pose_x = transform_rob.getOrigin().x();
+	pose_y = transform_rob.getOrigin().y();
+/*
+if(TEAM_COLOR == "CYAN"){
+	pose_x -= 2;
+} else {
+	pose_x += 2;
+}
+*/
     std::cout << "Coordenadas x: " << pose_x << " y: " << pose_y << std::endl;
 
     pose_ori = 0.0f;
