7bb90889fcfa69da788436f382bedd630041813b
Mitzi-968 <mitzi.ramest@gmail.com>
2024-07-19 13:23:42 -0600

    options for machines in aruco det srv -BK

--- PC_user/src/Vision/img_proc/scripts/aruco_det_srv2.py
+++ PC_user/src/Vision/img_proc/scripts/aruco_det_srv2.py
@@ -140,7 +140,14 @@ class FindTagNode:
                         aruco_pose.point.x, aruco_pose.point.y, aruco_pose.point.z = pos_z, -pos_y+0.21, -pos_x-0.25
                         print(aruco_pose.point.x, aruco_pose.point.y, aruco_pose.point.z, '\n')
 
-                      if(aruco_pose.point.x  < 4):
+                      #Para el C0
+                      #if(aruco_pose.point.x  < 4):
+
+                      #if para que solo detecte RS (grasping challenge)
+                      if(markerIds[i] == 111 or markerIds[i] == 112 or markerIds[i] == 113 or markerIds[i] == 114):
+                        
+                      #if para que solo detecte CS (grasping challenge)
+                      #if(markerIds[i] == 101 or markerIds[i] == 102 or markerIds[i] == 103 or markerIds[i] == 104):
 
                         start_point = (corneru[(0,0,0)],corneru[(0,0,1)])
 
