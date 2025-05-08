2da9804466c48dc5a64ad4a514498cc783c35df7
RafaCuellarR <rafael.cuellarr@ingenieria.unam.edu>
2025-05-08 08:35:01 -0600

    Training person correction

--- PC_user/src/festino_tools/src/FestinoVision.cpp
+++ PC_user/src/festino_tools/src/FestinoVision.cpp
@@ -102,7 +102,7 @@ bool FestinoVision::TrainingPerson(std::string person)
     std::cout << "FestinoVision.->Train person: " << person << std::endl;
     vision_msgs::FaceTrainSrv srv;
     std_msgs::String name_msg;
-    srv.request.name.data = person;
+    name_msg.data = person;
     srv.request.name = name_msg;
 
     if (cltTrainPersons.call(srv))
